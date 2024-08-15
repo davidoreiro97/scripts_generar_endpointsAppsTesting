7cf76e21d4a64246ed8cb90ff22fc6d39404d5c3
David oreiro <davidoreiro97@gmail.com>
2024-08-15 16:07:53 -0300

    Agregados scripts para iniciar los servidores

---
+++ scripts/iniciar_servidor_precios_backend_rosario_MAIN.ps1
@@ -0,0 +1,2 @@
+﻿#$host.ui.RawUI.WindowTitle = "Servidor Precios Backend Rosario main"
+npm --prefix A:\proyectos\BuscadorPreciosRosarioBackend\ run start
---
+++ scripts/iniciar_servidor_precios_backend_rosario_REDUNDANCIA.ps1
@@ -0,0 +1,2 @@
+﻿#$host.ui.RawUI.WindowTitle = "Servidor Precios Backend Rosario redundancia"
+npm --prefix A:\proyectos\BuscadorPreciosRosarioBackend_redundancia  run start
